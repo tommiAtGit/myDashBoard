223fd8a6ac32709df7e433a366310751378173eb
tommi-korteikko_agco <Tommi.Korteikko@agcocorp.com>
2024-11-22 17:33:16 +0200

    Added layout and css

--- myDashBoardWeb/mydasboard-app/src/components/myDashboardTodoView.js
+++ myDashBoardWeb/mydasboard-app/src/components/myDashboardTodoView.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './../App.css';
 
 const TodoView = () => {
     const [text1, setText1] = useState('');
@@ -7,16 +8,62 @@ const TodoView = () => {
     return (
         <div>
             <h2>Todo</h2>
-            <div>
-                <label>Text Field 1:</label>
-                <input type="text" value={text1} onChange={(e) => setText1(e.target.value)} />
+            <div class="row">
+                <div class="column-a">
+                    <div class="header-row">
+                        <h2>Column 1</h2>
+                    </div>
+                    <div class="card">
+                        <div class="container">
+                            <div class="title-container">
+                            <b>Some Title</b>
+                            </div>
+                            <div class="date-container">
+                                dd.mm.yyyy
+                            </div>
+                            <div class="clear-container"></div>
+                            <div class="content-row" >
+                                <p>Some content</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="container">
+                        <div class="title-container">
+                            <b>Some Other Title</b>
+                            </div>
+                            <div class="date-container">
+                                dd.mm.yyyy
+                            </div>
+                            <div class="clear-container"></div>
+                            <div class="content-row" >
+                                <p>Some other content</p>
+                            </div>
+                        </div>
+                    </div>
+
+                
+                </div>
+                <div class="column-b">
+                    <h2>Column 2</h2>
+                    <p>Some text..</p>
+                </div>
+                <div class="column-c">
+                    <h2>Column 3</h2>
+                    <p>Some text..</p>
+                </div>
+
             </div>
-            <div>
-                <label>Text Field 2:</label>
-                <input type="text" value={text2} onChange={(e) => setText2(e.target.value)} />
-            </div>
+           
         </div>
     );
 };
 
 export default TodoView;
+
+
+
+
+
+
+
