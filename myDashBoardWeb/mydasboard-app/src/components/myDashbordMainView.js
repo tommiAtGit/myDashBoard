c3d068f9e0a76721ec5a905069d7e2beb5311114
Tommi <tommi.korteikko@iCloud.com>
2024-12-03 20:17:00 +0200

    Added hower to side menu links

--- myDashBoardWeb/mydasboard-app/src/components/myDashbordMainView.js
+++ myDashBoardWeb/mydasboard-app/src/components/myDashbordMainView.js
@@ -10,7 +10,7 @@ const MainView = ({ children }) => {
             </div>
             <div style={{ display: 'flex' }}>
                 <SideMenu />
-                <div style={{ marginLeft: '10px', padding: '10px' }}>
+                <div style={{height:'100%', marginLeft: '10px', padding: '10px' }}>
                     {children}
                 </div>
             </div>
