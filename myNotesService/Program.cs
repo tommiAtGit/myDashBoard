bf07cdb58b80ee7b38e8d35c7f66fcce3481e880
Tommi <tommi.korteikko@iCloud.com>
2025-03-31 06:37:43 +0300

    Added Dockerfiles to finance and todoservices. Fixed program.cs files of the services

--- myNotesService/Program.cs
+++ myNotesService/Program.cs
@@ -14,6 +14,22 @@ builder.Services.AddScoped<IHealthService, HealthService>();
 // Register AutoMapper with DI
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+// Register custom services
+builder.Services.AddScoped<IHealthService, HealthService>();
+builder.Services.AddScoped<IGeneralNotesService, GeneralNotesService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Allow requests from the React app
+              .AllowAnyHeader() // Allow all headers
+              .AllowAnyMethod(); // Allow GET, POST, PUT, DELETE, etc.
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,9 +39,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
